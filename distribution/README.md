6cdb4bbc0a91315556d64dc26ce92dbf5f041254
miguelgrdotcom <miguel@miguelgr.com>
2020-05-15 11:17:04 +0200

    Distribution folder created

---
+++ distribution/README.md
@@ -0,0 +1,15 @@
+# Virtual Lab
+
+## What is Virtual Lab?
+
+## Dependencies
+
+## Installation
+
+## Configuring
+
+## Usage
+
+## More information
+
+* https://veyon.io/
---
+++ distribution/windows/Lecturer/install (run as administrator).bat
@@ -0,0 +1,11 @@
+@echo off
+cd %~dp0
+cd app
+veyon.exe /S /D=%programfiles%\Veyon
+"%programfiles%\Veyon\veyon-cli.exe" config clear
+"%programfiles%\Veyon\veyon-cli.exe" config set Authentication/Method 1
+"%programfiles%\Veyon\veyon-cli.exe" authkeys import tpp/public key.pem
+"%programfiles%\Veyon\veyon-cli.exe" authkeys import tpp/private private_key.pem
+"%programfiles%\Veyon\veyon-cli.exe" config upgrade
+cd..
+pause
