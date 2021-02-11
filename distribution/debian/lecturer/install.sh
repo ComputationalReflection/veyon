afb84b6d08553e80b6d331d8633313d3ebd47685
miguelgrdotcom <miguel@miguelgr.com>
2021-02-11 15:14:31 +0100

    Distribution files updated

--- distribution/debian/lecturer/install.sh
+++ distribution/debian/lecturer/install.sh
@@ -2,7 +2,9 @@
 cd app
 sudo dpkg -i veyon_4.3.5_amd64.deb
 sudo veyon-cli config clear
+sudo veyon-cli config import default.config
 sudo veyon-cli config set Authentication/Method 1
+sudo veyon-cli config set Master/ConfirmUnsafeActions true
 sudo veyon-cli authkeys import tpp/public key.pem
 sudo veyon-cli authkeys import tpp/private private_key.pem
 sudo veyon-cli config upgrade
--- distribution/debian/student/install.sh
+++ distribution/debian/student/install.sh
@@ -2,8 +2,10 @@
 cd app
 sudo dpkg -i veyon_4.3.5_amd64_NO_MASTER.deb
 sudo veyon-cli config clear
+sudo veyon-cli config import default.config
 sudo veyon-cli config set Service/Autostart false
 sudo veyon-cli config set Authentication/Method 1
+sudo veyon-cli config set Master/ConfirmUnsafeActions true
 sudo veyon-cli authkeys import tpp/public key.pem
 sudo veyon-cli config upgrade
 sudo veyon-cli service restart
