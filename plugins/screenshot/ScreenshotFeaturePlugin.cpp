c9aca477183dc188ae1f9b17a9fe3a34cf1e3105
Jose Quiroga <quirogajose@uniovi.es>
2023-03-10 11:38:34 +0100

    modificacion para generar capturas de pantalla de manera periodica y automatizada

--- plugins/screenshot/ScreenshotFeaturePlugin.cpp
+++ plugins/screenshot/ScreenshotFeaturePlugin.cpp
@@ -23,6 +23,7 @@
  */
 
 #include <QMessageBox>
+#include <QTimer>
 
 #include "ScreenshotFeaturePlugin.h"
 #include "ComputerControlInterface.h"
@@ -41,6 +42,9 @@ ScreenshotFeaturePlugin::ScreenshotFeaturePlugin( QObject* parent ) :
 								  QStringLiteral(":/screenshot/camera-photo.png") ) ),
 	m_features( { m_screenshotFeature } )
 {
+	m_autoShotEnabled = false;
+	m_autoTimer = new QTimer(this);
+	connect(m_autoTimer, SIGNAL(timeout()), this, SLOT(saveScreenshots()));
 }
 
 
@@ -51,25 +55,44 @@ const FeatureList &ScreenshotFeaturePlugin::featureList() const
 }
 
 
+void ScreenshotFeaturePlugin::saveScreenshots()
+{
+	if(m_autoShotEnabled)
+	{
+		for( const auto& controlInterface : m_lastComputerControlInterfaces )
+		{
+			Screenshot().take( controlInterface );
+		}
+	}
+}
+
 
 bool ScreenshotFeaturePlugin::startFeature( VeyonMasterInterface& master, const Feature& feature,
 											const ComputerControlInterfaceList& computerControlInterfaces )
 {
 	if( feature.uid() == m_screenshotFeature.uid() )
 	{
-		for( const auto& controlInterface : computerControlInterfaces )
+		m_lastComputerControlInterfaces = computerControlInterfaces;
+		if( m_autoShotEnabled == false)
 		{
-			Screenshot().take( controlInterface );
-
-		}
-
-		QMessageBox::information( master.mainWindow(),
-								  tr( "Screenshots taken" ),
+			QMessageBox::information( master.mainWindow(),
+								  tr( "Starting Auto Screenshots" ),
 								  tr( "Screenshot of %1 computer have been taken successfully." ).
 								  arg( computerControlInterfaces.count() ) );
-
+			int interval = 1000; //1 second?
+			m_autoTimer->start(interval);
+			m_autoShotEnabled = true;
+		}
+		else
+		{
+			m_autoTimer->stop();
+			m_autoShotEnabled = false;
+			QMessageBox::information(nullptr, tr("Stopping Auto Screenshots"), tr("Screenshots are now disabled"));
+		}
 		return true;
 	}
 
 	return true;
 }
+
+
